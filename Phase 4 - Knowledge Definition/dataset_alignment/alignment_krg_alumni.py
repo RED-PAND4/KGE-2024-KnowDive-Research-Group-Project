7e3c0816d62fb8381a5ce7363c40d92ca7bd068d
UnTardigrado5g <95749165+UnTardigrado5g@users.noreply.github.com>
2024-12-03 15:33:06 +0100

    Data alignment 2

---
+++ Phase 4 - Knowledge Definition/dataset_alignment/alignment_krg_alumni.py
@@ -0,0 +1,56 @@
+import json
+
+
+# Function to save the filtered data to a new file
+def save_filtered_data(filtered_data, output_file):
+    target_file = output_file + ".json"
+    with open(target_file, 'w') as file:
+        json.dump(filtered_data, file, indent=4)
+
+def drop_fields(source_json):
+    # Open and load the target JSON file
+    # with open(json_file, 'r') as file:
+    #    data = json.load(file)
+
+    with open(source_json, 'r') as file:
+        data = json.load(file)
+
+    new_data = []
+    for entry in data:
+        new_entry = {
+            "identifier": entry["id"],
+            "name": entry["name"],
+            "surname": entry["surname"],
+            "vcard:role": entry["role"],
+            "University": entry["university"],
+            "endDate": entry["endDate"]
+        }
+        new_data.append(new_entry)
+
+    return new_data
+
+def combine_and_translate (source_name, suffix):
+    final_data = []
+    for suff in suffix:
+        new_source_name = source_name + suff + ".json"
+        data_list = drop_fields(new_source_name)
+        for entry in data_list:
+            final_data.append(entry)
+
+    return final_data
+
+# Example usage
+sources = [
+    "./KRG-UNITN-alumni"
+]
+suffix = [""]
+
+outputs = [
+    "./Aligned-KRG-UNITN-alumni"
+]
+
+if len(sources) == len(outputs):
+    for i in range(len(sources)):
+        to_save = combine_and_translate(sources[i], suffix)
+        save_filtered_data(to_save, outputs[i])
+        print("Saved"+str(i))
